cc4b137d78f2b22c2f87aecc304283ecb72c3aa9
Christopher Paepe <cmpsbh@gmail.com>
2013-11-12 14:05:57 -0500

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rainforest::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,6 +56,9 @@ Rainforest::Application.routes.draw do
   #     resources :products
   #   end
   Rainforest::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   resources :products
+  resources :users, :only => [:new, :create]
 end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -6,17 +6,24 @@
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and 2unsustainable approach (the more migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111192355) do
+ActiveRecord::Schema.define(version: 20131112180200) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
